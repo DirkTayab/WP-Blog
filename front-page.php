56ae8634e60d8350f07b889d7f4ffa475c4bead5
Chryztellevatorr <lhancesantiago0@gmail.com>
2024-08-28 15:55:26 +0800

    first commit

---
+++ front-page.php
@@ -0,0 +1,46 @@
+<?php get_header() ?>
+
+<section class="banner bg--dark clr--light">
+      <div class="container">
+        <div class="banner__wrapper">
+          <h1 class="banner__title">The Blog</h1>
+          <div class="banner__article">
+            <div class="banner__grid">
+              <div class="banner__primary">
+                <div class="card__banner__lg">
+                  <img src="./images/banner_primary.jpg" alt="" />
+                  <ul class="card__meta flex">
+                    <li class="article__date">May 20, 2002</li>
+                  </ul>
+                  <p class="article__excerpt">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Sapiente, ratione quia? Velit labore rerum doloremque
+                    eligendi? Quidem quas doloremque perspiciatis itaque facilis
+                    delectus illum libero!
+                  </p>
+                  <a href="# " class="article__more">Read More</a>
+                </div>
+              </div>
+              <div class="banner__secondary">
+                <div class="card__banner__sm">
+                  <div class="flex">
+                    <img src="./images/banner_sub1.jpg" alt="" />
+                    <div class="wrapper">
+                      <ul class="card__meta flex">
+                        <li class="article__date">May 20, 2002</li>
+                      </ul>
+                      <h3>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit
+                      </h3>
+                      <a href="#">Read More</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<?php get_footer() ?>
